367c1c1d958a0502cbc46a53f678c19fe446e289
aadebuger <zhvxxh@gmail.com>
2021-02-14 23:15:13 +0800

    first commit

---
+++ studentmonitor.py
@@ -0,0 +1,73 @@
+import adbutils
+
+
+#/Users/aadebuger/Library/Android/sdk/platform-tools
+def init_leancloud_client():
+    import os
+
+    LEANCLOUD_APP_ID = os.environ.get("LEANCLOUD_APP_ID", "rGngnUit9fqERRVjQMfzQhWg-gzGzoHsz")
+    LEANCLOUD_APP_KEY = os.environ.get("LEANCLOUD_APP_KEY", "xWQ3c4CoLPXIlRd6UxLRGndX")
+    LEANCLOUD_MASTER_KEY = os.environ.get("LEANCLOUD_MASTER_KEY", "x3cl6OYR2mC6dDQsW0dMeceJ")
+    LEANCLOUD_REGION = os.environ.get("LEANCLOUD_REGION", "CN")
+    leancloud.init(app_id=LEANCLOUD_APP_ID, app_key=LEANCLOUD_APP_KEY, master_key=LEANCLOUD_MASTER_KEY)
+    leancloud.use_region(LEANCLOUD_REGION)
+    print("leancloud init success with app_id: {}, app_key: {}, region: {}".format(LEANCLOUD_APP_ID, LEANCLOUD_APP_KEY,
+                                                                                   LEANCLOUD_REGION))
+
+def newAndroiddevice(serial):
+    TestObject = leancloud.Object.extend('Androiddevice')
+    test_object = TestObject()
+    test_object.set('serial',serial)
+
+    test_object.save()
+    print(test_object)
+def updateAndroiddevice(androido,serial):
+
+    androido.set('serial',serial)
+
+    androido.save()
+    print(androido)
+    
+def studentlist():
+    Todo = leancloud.Object.extend('Student')
+    query = Todo.query
+    query.equal_to('synciing', '1')
+    query_result = query.find()
+    conv=[]
+    for item in query_result:
+        print(item)
+        value=encode(item,dump_objects=True)
+        print(value)
+        conv.append(item)
+    return list(conv)
+
+def studentsync(item):
+        print("sync")
+        print(item)
+def monitorstudent():
+        slist = studentlist()
+        map(lambda student:studentsync(student), list(slist))
+
+
+    
+
+def montiorlesson(lesson):
+		serialv= checked(lesson)
+		if len(serialv)==0:
+			return 
+		
+
+
+#kangding
+import os
+import json
+import leancloud
+from leancloud.utils import encode
+#os.environ.setdefault('LEANCLOUD_API_SERVER', "http://localhost:5000")
+
+
+os.environ['LEANCLOUD_API_SERVER'] = os.environ.get('LEANCLOUD_API_SERVER',"http://192.168.31.82:7000")
+
+init_leancloud_client()
+
+monitorstudent()
