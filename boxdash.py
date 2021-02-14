38816f0d6ee50ba7395ddb149317058cd3c6f5b9
aadebuger <zhvxxh@gmail.com>
2021-02-15 00:32:56 +0800

    person

--- boxdash.py
+++ boxdash.py
@@ -48,8 +48,8 @@ def personstatus():
         "data": json.dumps(datav)
     }
     response = requests.post(url, data=json.dumps(payload), headers=headers).text
-    print(response)	
-
+    for item in r["data"]:
+        print(item)
 def startMonitor():
 #    scheduler.add_job(event_monitor,'interval', minutes=1) 
     scheduler.add_job(boxstatus,'interval', seconds=20) 
