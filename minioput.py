87fbe2ed2c4962c794089a98ff167ed993fc7c2f
aadebuger <zhvxxh@gmail.com>
2021-02-14 14:47:43 +0800

    first commit

---
+++ minioput.py
@@ -0,0 +1,25 @@
+from minio import Minio
+#from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+#                         BucketAlreadyExists)
+
+# Initialize minioClient with an endpoint and access/secret keys.
+from minio.error import InvalidResponseError
+minioClient = Minio('localhost:9000',
+                    access_key='minioadmin',
+                    secret_key='minioadmin',
+                    secure=False)
+
+
+#try:
+#    minioClient.make_bucket("boxhr")
+#except InvalidResponseError as err:
+#    print(err)
+# Put an object 'pumaserver_debug.log' with contents from 'pumaserver_debug.log'.
+#try:
+#       minioClient.fput_object('androidimg', '20200119140300012.png', '2060S.png')
+#except InvalidResponseError as err:
+#       print(err)
+try:
+       minioClient.fput_object('boxhr', 'test.jpeg', 'test.jpeg')
+except InvalidResponseError as err:
+       print(err)
