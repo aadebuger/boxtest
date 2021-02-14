23a566701ff8991fc54c2f8e29082260a1f1f721
aadebuger <zhvxxh@gmail.com>
2021-02-14 10:34:37 +0800

    first commit

---
+++ boxstudent.py
@@ -0,0 +1,20 @@
+import requests
+
+
+#url="http://192.168.124.43:8882/sendData"
+url="http://192.168.124.43:8088/sendData"
+
+import json
+headers = {
+    "Content-Type": "application/json; charset=UTF-8"
+    }
+payload={
+	"serialNumber": "068bebf627d6ab24",
+	"devicepass": "123456",
+	"tasktype": "7",
+	"data": "[0,10,0,0]"
+}
+
+response = requests.post(url, data=json.dumps(payload), headers=headers).text
+print(response)
+print("hello")
