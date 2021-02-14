243f48c35de442c0c129eb2def221a19c5ed991d
aadebuger <zhvxxh@gmail.com>
2021-02-14 18:59:05 +0800

    more test

--- anroidmonitor.sh
+++ anroidmonitor.sh
@@ -2,4 +2,4 @@ export LEANCLOUD_API_SERVER=http://192.168.1.17:7000
 cd /home/ubuntu/boxtest
 sleep 30
 source ~/jncloudvenv3/bin/activate
-nohup python androidmonitor.py &
+nohup python androidmonitor.py>>amonitor.out  2>&1 &
