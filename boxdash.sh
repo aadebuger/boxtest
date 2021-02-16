5c7640415adb8991a10244a3653c647081f47151
aadebuger <zhvxxh@gmail.com>
2021-02-16 14:52:45 +0800

    py

--- boxdash.sh
+++ boxdash.sh
@@ -2,4 +2,4 @@ export LEANCLOUD_API_SERVER=http://192.168.124.48:7000
 cd /home/ubuntu/boxtest
 sleep 30
 source ~/jncloudvenv3/bin/activate
-nohup python androidmonitor.py>>boxdash.out  2>&1 &
+nohup python boxdash.py>>boxdash.out  2>&1 &
