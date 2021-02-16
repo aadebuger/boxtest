449f08ef2ba1fc5535eb73d7dc02e36a2deba6c4
aadebuger <zhvxxh@gmail.com>
2021-02-16 14:37:29 +0800

    sync api

--- boxdash.py
+++ boxdash.py
@@ -5,6 +5,68 @@ import base64
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
+import arrow
+from leancloud.utils import encode
+def init_leancloud_client():
+    import os
+
+    LEANCLOUD_APP_ID = os.environ.get("LEANCLOUD_APP_ID", "rGngnUit9fqERRVjQMfzQhWg-gzGzoHsz")
+    LEANCLOUD_APP_KEY = os.environ.get("LEANCLOUD_APP_KEY", "xWQ3c4CoLPXIlRd6UxLRGndX")
+    LEANCLOUD_MASTER_KEY = os.environ.get("LEANCLOUD_MASTER_KEY", "x3cl6OYR2mC6dDQsW0dMeceJ")
+    LEANCLOUD_REGION = os.environ.get("LEANCLOUD_REGION", "CN")
+    leancloud.init(app_id=LEANCLOUD_APP_ID, app_key=LEANCLOUD_APP_KEY, master_key=LEANCLOUD_MASTER_KEY)
+    leancloud.use_region(LEANCLOUD_REGION)
+    print("leancloud init success with app_id: {}, app_key: {}, region: {}".format(LEANCLOUD_APP_ID, LEANCLOUD_APP_KEY,
+                                                                                   LEANCLOUD_REGION))
+
+def newBox(boxdata):
+
+    TestObject = leancloud.Object.extend('Box')
+    test_object = TestObject()
+    test_object.set('boxNumber',boxdata['boxNumber'])
+    test_object.set('personiidArray',json.loads(boxdata['personidArray']))
+    test_object.set('saved',boxdata['saved'])     
+                    
+    test_object.set('state',boxdata['state'])
+    test_object.set('type',boxdata['type'])  
+    boxdate =arrow.get(boxdata['time'])
+    boxdate1 = boxdate.to('Asia/Shanghai')
+    test_object.set('boxtime',boxdate1.datetime)                
+    test_object.save()
+    print(test_object)
+def updateBox(test_object,boxdata):
+
+    test_object.set('boxNumber',boxdata['boxNumber'])
+    test_object.set('personiidArray',json.loads(boxdata['personidArray']))
+    test_object.set('saved',boxdata['saved'])     
+                    
+    test_object.set('state',boxdata['state'])
+    test_object.set('type',boxdata['type'])  
+    boxdate =arrow.get(boxdata['time'])
+    boxdate1 = boxdate.to('Asia/Shanghai')
+    test_object.set('boxtime',boxdate1.datetime)                
+    test_object.save()
+    print(test_object)
+def boxlist(boxNumber):
+    Todo = leancloud.Object.extend('Box')
+    query = Todo.query
+    query.equal_to('boxNumber', boxNumber)
+
+    query_result = query.find()
+    conv=[]
+    for item in query_result:
+        print(item)
+        value=encode(item,dump_objects=True)
+        print(value)
+        conv.append(item)
+    return list(conv)
+
+def boxmonitor(boxNumber,boxdata):
+     boxv = boxlist(boxNumber)
+     if len(boxv)==0:
+        newBox(boxdata)
+     else:
+        updateBox(boxv[0],boxdata)
 #url="http://192.168.124.43:8882/sendData"
 url="http://192.168.124.43:8088/sendData"
 
@@ -37,6 +99,8 @@ def boxstatus():
     for item in r["data"]:
         print(item)
     print("hello")
+    retv= map(lambda item:boxmonitor(item['boxNumber'],item),r['data'])
+    print(list(retv))
 def personstatus():
     todaydate = arrow.utcnow()
     todaydate=todaydate.to('Asia/Shanghai')
@@ -50,6 +114,9 @@ def personstatus():
         "data": json.dumps(datav)
     }
     response = requests.post(url, data=json.dumps(payload), headers=headers).text
+    with open('./persontatus.json', 'w') as json_file:
+        json_file.write(response)
+
     r = json.loads(response)
     for item in r["data"]:
         print("p=",item)
@@ -61,6 +128,13 @@ def startMonitor():
 
     scheduler.daemonic = False 
     scheduler.start()
+import leancloud
+from leancloud.utils import encode
+#os.environ.setdefault('LEANCLOUD_API_SERVER', "http://localhost:5000")
+#os.environ.setdefault('LEANCLOUD_API_SERVER', "http://192.168.31.82:7000")
+#print(os.environ.get('LEANCLOUD_API_SERVER'))
+os.environ['LEANCLOUD_API_SERVER'] = os.environ.get('LEANCLOUD_API_SERVER',"http://192.168.31.82:7000")
+init_leancloud_client()
 scheduler = BackgroundScheduler()
 startMonitor()
 time.sleep(1000000000)
