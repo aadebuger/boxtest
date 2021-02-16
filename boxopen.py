4200eefcd5016073dd4b996a57268e2fecc9be1d
aadebuger <zhvxxh@gmail.com>
2021-02-16 11:37:58 +0800

    first commit

---
+++ boxopen.py
@@ -0,0 +1,23 @@
+url="http://192.168.124.43:8088/sendData"
+
+import json
+import sys
+
+headers = {
+    "Content-Type": "application/json; charset=UTF-8"
+    }
+def openbox(i):
+    boxv=[i]
+    payload={
+        "serialNumber": "068bebf627d6ab24",
+        "devicepass": "123456",
+        "tasktype": "24",
+        "data": json.dumps(boxv)
+    }
+    response = requests.post(url, data=json.dumps(payload), headers=headers).text
+    print(response)
+    print("hello")
+if __name__ == "__main__":
+    # execute only if run as a script
+    openbox(sys.argv[1])
+
