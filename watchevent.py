2b1a4696b171aaff2d6fa6ca1865b42620511155
aadebuger <zhvxxh@gmail.com>
2021-03-02 22:20:34 +0800

    action

--- watchevent.py
+++ watchevent.py
@@ -13,6 +13,7 @@ def getMydbip():
 
     return "mongodb://root:root13906917736@{0}:27017/".format(os.environ.get('MYDB_PORT_27017_TCP_ADDR',"127.0.0.1"))
 
+print("watch event ..........\n")
 client1 = MongoClient(getMydbip())
 def getMclient():
          return client1
@@ -20,5 +21,13 @@ client = pymongo.MongoClient(getMydbip())
 change_stream = client.test_database.event.watch()
 for change in change_stream:
     print(dumps(change))
+    print("change=",change["_id"])
     print('') # for readability only
+    operationType=change["operationType"]
+    documentKey=change["documentKey"]
+    print("oiid=",documentKey["_id"])
+    print("oid=",str(documentKey["_id"]))
+    action = change['fullDocument']["action"]
+    print("action=",action)
+    print("change ovoer\n")
 #test_database
