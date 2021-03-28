04e015c0d0e49ab47a305d50ccfde23fd910d906
aadebuger <zhvxxh@gmail.com>
2021-03-28 11:21:37 +0800

    BOX_ID

--- boxmonitor.sh
+++ boxmonitor.sh
@@ -2,4 +2,5 @@ export LEANCLOUD_API_SERVER=http://localhost:7000
 cd /home/ubuntu/boxtest
 sleep 30
 source ~/jncloudvenv3/bin/activate
+export BOX_ID=3dcc6b61375ee359
 nohup python boxmonitor.py>>boxmonitor.out  2>&1 &
